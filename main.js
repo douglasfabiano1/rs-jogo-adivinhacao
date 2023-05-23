3833aff18c298a0ebf4414f42c3da32e7da05c91
Douglas Fabiano <douglasfabiano23@gmail.com>
2023-05-23 10:32:51 -0300

    refactoring

--- main.js
+++ main.js
@@ -1,32 +1,46 @@
+// Variables
 const screen1 = document.querySelector(".screen1");
 const screen2 = document.querySelector(".screen2");
-
-const randomNumber = Math.round(Math.random() * 10);
+const btnTry = document.querySelector("#btnTry");
+const btnReset = document.querySelector("#btnReset");
+let randomNumber = Math.round(Math.random() * 10);
 let xAttemps = 1;
 
-// Callback fuction
+// Events
+btnTry.addEventListener("click", handleTryClick);
+btnReset.addEventListener("click", handleResetClick);
+document.addEventListener("keypress", handleEnterPress);
+
+// Callback fuctions
 function handleTryClick(event) {
   event.preventDefault();
 
   const inputNumber = document.querySelector("#inputNumber");
 
   if (Number(inputNumber.value) == randomNumber) {
-    screen1.classList.toggle("hide");
-    screen2.classList.toggle("hide");
-    screen2.querySelector("h2")
-        .innerText = `Acertou em ${xAttemps} tentativas!`;
+    toggleScreen();
+    screen2.querySelector(
+      "h2"
+    ).innerText = `Acertou em ${xAttemps} tentativas!`;
   }
+
   inputNumber.value = "";
   xAttemps++;
 }
 
-// Events
-const btnTry = document.querySelector("#btnTry");
-const btnReset = document.querySelector("#btnReset");
+function handleResetClick() {
+  toggleScreen();
+  xAttemps = 1;
+  randomNumber = Math.round(Math.random() * 10);
+}
 
-btnTry.addEventListener("click", handleTryClick);
-btnReset.addEventListener("click", function () {
+function toggleScreen() {
   screen1.classList.toggle("hide");
   screen2.classList.toggle("hide");
-  xAttemps = 1;
-});
+}
+
+function handleEnterPress(event){
+  if (event.key === "Enter" && screen1.classList.contains("hide")) {
+    handleResetClick();
+  }
+}
